a2ea28ddc60b1033481d71c2a59f48d2f498dd77
pooja pangarekar <pangrekarpooja@gmail.com>
2023-11-27 14:10:05 +0530

    first commit

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload multiple images</title>
+    <style>
+        body{
+         background-color:lightblue;
+         text-align:center;
+         margin-top: 10%;
+
+        }
+
+
+        </style>
 </head>
 <body>
+
+    <h1>PHP File Upload</h1>
+
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <input type="file" name="myImages[]" multiple /><br> <br>
         <input type="submit" name="submit" value="upload images">
